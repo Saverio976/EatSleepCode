6bbdce54e045e4909457ec9c7fd929a21f0885e8
Saverio Mishta <personex976@gmail.com>
2021-10-22 18:58:35 +0200

    [add] syntax higthligth for C language keyword and type; [update] use context_t struct for window, text and font

---
+++ include/struct_context.h
@@ -0,0 +1,20 @@
+/*
+** Saverio976, 2021
+** EatSleepCode
+** File description:
+** Context window with font, window, text
+*/
+
+#ifndef CONTEXT_H_
+    #define CONTEXT_H_
+
+    #include <SFML/Graphics.h>
+
+struct context {
+    sfRenderWindow *window;
+    sfFont *font;
+    sfText *text;
+};
+typedef struct context context_t;
+
+#endif
