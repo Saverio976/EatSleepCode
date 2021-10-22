c92ba29d95bd8f15d8e2f5860c655b7a3a8157c7
Saverio Mishta <personex976@gmail.com>
2021-10-22 18:56:19 +0200

    add Color for C language keyword and type

--- include/constant.h
+++ include/constant.h
@@ -8,8 +8,13 @@
 #ifndef CONSTANT_H_
     #define CONSTANT_H_
 
+    #include <SFML/Graphics.h>
+
 //    #define FONT_PATH "assets/LiberationMono-Regular.ttf"
     #define FONT_PATH "assets/SourceCodePro-MediumIt.otf"
     #define BG_PATH "assets/black_bg.png"
+    #define KYW_COLOR sfYellow
+    #define TYP_COLOR sfGreen
+    #define DFL_COLOR sfWhite
 
 #endif
