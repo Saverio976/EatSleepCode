71b0faa1790f7858990c7f6bc75a2823bdd9d78f
Saverio Mishta <personex976@gmail.com>
2021-10-24 01:42:07 +0200

    [update] lib/my/my_strncpy.c: test with vlagrind and update potential unitialised value

--- lib/my/my_strncpy.c
+++ lib/my/my_strncpy.c
@@ -5,16 +5,13 @@
 ** copy src in dest with n char
 */
 
-int my_strlen(char const *);
-
 char *my_strncpy(char *dest, char const *src, int n)
 {
     int i;
-    int len = my_strlen(src);
 
-    for (i = 0; i < n && i < len; i++)
+    for (i = 0; i < n && src[i] != '\0'; i++)
         dest[i] = src[i];
-    if (n > len)
+    if (n > i)
         dest[i] = '\0';
     return dest;
 }
