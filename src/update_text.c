bca0355fecc802522d4b514bf6c0bca22bc275b7
Saverio Mishta <personex976@gmail.com>
2021-10-24 16:13:58 +0200

    [update] src/update_text.c: save_swap_modif remove the write(fd newline) because the cache have \n at each lien

--- src/update_text.c
+++ src/update_text.c
@@ -21,16 +21,11 @@ void save_text_modif(context_t *ctx)
 void save_swap_modif(context_t *ctx)
 {
     int fd = open(ctx->swap, O_WRONLY | O_TRUNC);
-    int i = 0;
-    char newline = '\n';
 
     if (fd < 0)
         write_file_error(ctx->swap, ": Error when saving modified data");
-    while (ctx->text_file[i] != NULL) {
+    for (int i = 0; ctx->text_file[i] != NULL; i++)
         write(fd, ctx->text_file[i], my_strlen(ctx->text_file[i]));
-        write(fd, &newline, 1);
-        i++;
-    }
     close(fd);
 }
 
